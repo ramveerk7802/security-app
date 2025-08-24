03d3977445bc2a2f121a3807c5a4e9db27d2d6f5
Ramveer <ramveerk7802@gmail.com>
2025-08-24 21:09:10 +0530

    Changed UI Enable the ClipBoard accessibility

--- app/src/main/java/com/rvcode/securityapp/screens/PermissionScannerScreen.kt
+++ app/src/main/java/com/rvcode/securityapp/screens/PermissionScannerScreen.kt
@@ -4,19 +4,24 @@ import android.content.Context
 import android.content.Intent
 import android.provider.Settings
 import android.util.Log
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -24,8 +29,10 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 
@@ -122,17 +129,25 @@ private fun AppList(riskyApps: List<AppWithRisk>) {
 }
 
 
+@Preview
 @Composable
 private fun EnableServicePrompt(){
     val context = LocalContext.current
-    Column(
+    OutlinedCard(
+        modifier = Modifier.fillMaxWidth().padding(12.dp),
+        shape = RoundedCornerShape(16.dp),
+        border = BorderStroke(
+            width = 2.dp,
+            color = Color.Blue
+        )
+    ){Column(
         modifier = Modifier.padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
         Text(
             text = "For phishing protection, you must enable the clipboard monitoring service.",
-            textAlign = TextAlign.Center
+            style = MaterialTheme.typography.labelLarge
         )
         Spacer(modifier = Modifier.height(8.dp))
         ElevatedButton(onClick = {
@@ -142,5 +157,7 @@ private fun EnableServicePrompt(){
         }) {
             Text("Enable in Settings")
         }
+    }
+
     }
 }
